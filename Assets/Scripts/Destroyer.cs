1cff808c6dbfc798373cf91cffd691a77b8c7e34
Luis de la Cal <luiscal4a@gmail.com>
2018-10-19 11:52:05 +0200

    Script destroyer destruye suelo que ya no es visible

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
