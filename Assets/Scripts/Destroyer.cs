f659c175980e655a4d3e2e921d6e3e165d7f3c4f
Luis de la Cal <luiscal4a@gmail.com>
2018-10-21 16:35:26 +0200

    Añadida funcionalidad de creación de prefabs infinita para endless runner

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -4,24 +4,46 @@ using UnityEngine;
 
 public class Destroyer : MonoBehaviour {
 
-    public GameObject myPrefab;
+    public static GameObject[] myObjects;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject myGobj;
+
+    // Use this for initialization
+    void Start () {
+        myObjects = Resources.LoadAll<GameObject>("Prefabs");
+        myInstantiation();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag == "Ground")
+        if (col.gameObject.tag == "Ground"|| col.gameObject.tag == "Platform")
         {
             Destroy(col.gameObject);
-            Instantiate(myPrefab, new Vector2(17, 0), Quaternion.identity);
+            //Instantiate(myPrefab, new Vector2(17, 0), Quaternion.identity);
         }
+        else if (col.gameObject.tag == "prefab")
+        {
+            BoxCollider2D bc2d = col.gameObject.GetComponent<BoxCollider2D>();
+            float width = bc2d.bounds.size.x;
+            Destroy(col.gameObject);
+            myInstantiation();
+        }
+    }
+
+    void myInstantiation()
+    {
+        int whichItem = Random.Range(0, myObjects.Length);
+        BoxCollider2D bc2d = myGobj.GetComponent<BoxCollider2D>();
+        float width = bc2d.bounds.size.x;
+        GameObject mygob = Instantiate(myObjects[whichItem], new Vector2(myGobj.transform.position.x+width+5, 0), Quaternion.identity);
+        myGobj = mygob;
+        bc2d = mygob.GetComponent<BoxCollider2D>();
+        width = bc2d.bounds.size.x;
+        Debug.Log(width);
+        transform.position = new Vector2(-14-width, transform.position.y);
     }
 }
