8df8efa8e65029323dd0254973e4bda771fba116
Luis de la Cal <luiscal4a@gmail.com>
2018-10-27 23:50:15 +0200

    Background temporal añadido y mejora en sorting layers

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -38,7 +38,7 @@ public class Destroyer : MonoBehaviour {
         int whichItem = Random.Range(0, myObjects.Length);
         BoxCollider2D bc2d = myGobj.GetComponent<BoxCollider2D>();
         float width = bc2d.bounds.size.x;
-        transform.position = new Vector2(-14 - width, transform.position.y);
+        transform.position = new Vector2(-18 - width, transform.position.y);
         GameObject mygob = Instantiate(myObjects[whichItem], new Vector2(myGobj.transform.position.x+width+5, 0), Quaternion.identity);
         myGobj = mygob;
         bc2d = mygob.GetComponent<BoxCollider2D>();
